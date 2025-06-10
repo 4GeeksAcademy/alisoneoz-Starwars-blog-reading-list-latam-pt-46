93f929c4c5903c2fbeda49152dbd660c91c2c4f4
alisoneoz <alisonestephanyoz@gmail.com>
2025-06-10 11:59:52 -0400

    add favorites to the dropdown list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
 import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
 	const { store } = useGlobalReducer()
-
+  
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
@@ -11,17 +12,22 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">Star Wars</span>
 				</Link>
 				
-
-
-
 				<div className="dropdown">
 					<button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-					Favorites ({store.favorites.length} )
+					Favorites ({store.favorites.length})
 					</button>
 					<ul className="dropdown-menu">
-						<li><a className="dropdown-item" href="#">Action</a></li>
-						<li><a className="dropdown-item" href="#">Another action</a></li>
-						<li><a className="dropdown-item" href="#">Something else here</a></li>
+						{store.favorites.length === 0 
+							? (<li>(Empty!)</li>) 
+							: (
+								store.favorites.map((favoriteItem)=>(
+									<li  className="dropdown-item" key={favoriteItem.uid}>
+										<div className="">{favoriteItem.name} <FaTrash /></div>
+										
+									</li>
+								))
+							)
+						}					
 					</ul>
 				</div>
 			</div>
